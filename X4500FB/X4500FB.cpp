2680d2df783d8005decaae0863202f8ea73a4ecb
george albert <albert@george-albertmatoMacBook.local>
2014-03-03 14:02:10 +0800

    add IOFrameBuffer, IOGraphics 3.9 is version 2.2 for 10.6

--- X4500FB/X4500FB.cpp
+++ X4500FB/X4500FB.cpp
@@ -1,1 +1,77 @@
-/* add your code here */
+#include <IOKit/IOLib.h>
+
+#include "X4500FB.h"
+
+
+
+// This required macro defines the class's constructors, destructors,
+
+// and several other methods I/O Kit requires.
+
+OSDefineMetaClassAndStructors(X4500, IOService)
+
+
+
+// Define the driver's superclass.
+
+#define super IOService
+
+
+bool X4500::init(OSDictionary *dict)
+{
+    bool result = super::init(dict);
+    
+    IOLog("Initializing\n");
+    
+    return result;
+}
+
+
+
+void X4500::free(void)
+{
+    IOLog("Freeing\n");
+    super::free();
+}
+
+
+
+IOService *X4500::probe(IOService *provider, SInt32 *score)
+
+{
+    
+    IOService *result = super::probe(provider, score);
+    
+    IOLog("Probing\n");
+    
+    return result;
+    
+}
+
+
+
+bool X4500::start(IOService *provider)
+
+{
+    
+    bool result = super::start(provider);
+    
+    IOLog("Starting\n");
+    
+    return result;
+    
+}
+
+
+
+void X4500::stop(IOService *provider)
+
+{
+    
+    IOLog("Stopping\n");
+    
+    super::stop(provider);
+    
+}
+
+
--- X4500FB/X4500FB.h
+++ X4500FB/X4500FB.h
@@ -1,1 +1,28 @@
-/* add your code here */
+#include <IOKit/IOService.h>
+#include <IOKit/graphics/IOFrameBuffer.h>
+
+class X4500 : public IOService
+
+{
+    
+    OSDeclareDefaultStructors(X4500)
+    
+public:
+    
+    virtual bool init(OSDictionary *dictionary = 0);
+    
+    virtual void free(void);
+    
+    virtual IOService *probe(IOService *provider, SInt32 *score);
+    
+    virtual bool start(IOService *provider);
+    
+    virtual void stop(IOService *provider);
+    
+};
+
+
+class X4500FB : public IOFramebuffer
+{
+    OSDeclareDefaultStructors(X4500FB)
+};
