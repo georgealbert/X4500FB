13c4267a3de2deee8612b8a91895100f8d5a3229
Albert <georgealbert@qq.com>
2014-03-05 21:25:47 +0800

    for github

--- X4500FB/X4500FB.cpp
+++ X4500FB/X4500FB.cpp
@@ -1,4 +1,5 @@
 #include <IOKit/IOLib.h>
+#include <IOKit/ndrvsupport/IOMacOSTypes.h>
 #include <IOKit/graphics/IOFrameBuffer.h>
 #include "X4500FB.h"
 
@@ -49,8 +50,7 @@ IOService *X4500::probe(IOService *provider, SInt32 *score)
 }
 
 bool X4500::start(IOService *provider)
-{
-    
+{    
     bool result = super::start(provider);   
     IOLog("Starting\n");
     return result;
@@ -81,3 +81,73 @@ IOReturn X4500FB::getDisplayModes(IODisplayModeID *allDisplayModes)
     return kIOReturnSuccess;
 }
 
+/*
+ * copy from IONDRVFramebuffer.cpp
+ */
+const char * getPixelFormats( void )
+{
+    static const char * ndrvPixelFormats =
+    IO1BitIndexedPixels "\0"
+    IO2BitIndexedPixels "\0"
+    IO4BitIndexedPixels "\0"
+    IO8BitIndexedPixels "\0"
+    IO16BitDirectPixels "\0"
+    IO32BitDirectPixels "\0"
+    "\0";
+    
+    return (ndrvPixelFormats);
+}
+
+IOReturn getPixelInformation( IODisplayModeID displayMode, IOIndex depth, IOPixelAperture aperture, IOPixelInformation *pixelInfo )
+{
+    //PE_Video    bootDisplay;
+    
+    if (aperture || depth )
+        return (kIOReturnUnsupportedMode);
+    
+    //getPlatform()->getConsoleInfo( &bootDisplay);
+    
+    bzero( info, sizeof( *info));
+    
+    info->activeWidth           = 1280;
+    info->activeHeight          = 800;
+    info->bytesPerRow           = bootDisplay.v_rowBytes & 0x7fff;
+    info->bytesPerPlane         = 0;
+    
+    switch (bootDisplay.v_depth)
+    {
+        case 8:
+        default:
+            strlcpy(info->pixelFormat, IO8BitIndexedPixels, sizeof(info->pixelFormat));
+            info->pixelType             = kIOCLUTPixels;
+            info->componentMasks[0]     = 0xff;
+            info->bitsPerPixel          = 8;
+            info->componentCount        = 1;
+            info->bitsPerComponent      = 8;
+            break;
+        case 15:
+        case 16:
+            strlcpy(info->pixelFormat, IO16BitDirectPixels, sizeof(info->pixelFormat));
+            info->pixelType     = kIORGBDirectPixels;
+            info->componentMasks[0] = 0x7c00;
+            info->componentMasks[1] = 0x03e0;
+            info->componentMasks[2] = 0x001f;
+            info->bitsPerPixel  = 16;
+            info->componentCount        = 3;
+            info->bitsPerComponent      = 5;
+            break;
+        case 24:
+        case 32:
+            strlcpy(info->pixelFormat, IO32BitDirectPixels, sizeof(info->pixelFormat));
+            info->pixelType     = kIORGBDirectPixels;
+            info->componentMasks[0] = 0x00ff0000;
+            info->componentMasks[1] = 0x0000ff00;
+            info->componentMasks[2] = 0x000000ff;
+            info->bitsPerPixel  = 32;
+            info->componentCount        = 3;
+            info->bitsPerComponent      = 8;
+            break;
+    }
+    
+    return (kIOReturnSuccess);
+}
